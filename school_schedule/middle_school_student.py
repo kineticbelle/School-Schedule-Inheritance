2688e3d0b77d7c632c59221469f0ae1150d39bd4
Natasha Gaye <natashagaye11@gmail.com>
2025-04-02 13:33:03 -0500

    completed activity, pushing commits from Laura + Natasha

--- school_schedule/middle_school_student.py
+++ school_schedule/middle_school_student.py
@@ -1,3 +1,17 @@
 from .student import Student
 
 # add MiddleSchoolStudent here
+class MiddleSchoolStudent(Student):
+    def __init__(self, name, grade, classes, gets_transportation = True):
+        super().__init__(name, grade, classes)
+        self.gets_transportation = gets_transportation
+
+    def summary(self):
+        student_summary = super().summary()
+        
+        if not self.gets_transportation:
+            student_summary += ", doesn't get transportation."
+        else:
+            student_summary += ", get's transportation."
+        
+        return student_summary
